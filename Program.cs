375c4d281c89b00de1a0c2a6dfaac0bf65bbd0ca
SegZWealth <41188367+SegZWealth@users.noreply.github.com>
2019-03-07 08:29:59 +0100

    Prime number

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+
+namespace interleave2arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 2; i < 100; i++)
+            {
+                var val = chkprime(i);
+                if (val)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+            Console.ReadLine();
+
+        }
+
+        public static bool chkprime(int num)
+        {
+            for (int i = 2; i < num; i++)
+                if (num % i == 0)
+                    return false;
+            return true;
+        }
+    }
+}
