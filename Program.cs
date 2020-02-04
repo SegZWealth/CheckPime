ec88203b7e92e8ca778b614147ceec9685b47180
SegZWealth <41188367+SegZWealth@users.noreply.github.com>
2020-02-04 18:54:24 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -22,7 +22,7 @@ namespace interleave2arrays
 
         public static bool chkprime(int num)
         {
-            for (int i = 2; i < num; i++)
+            for (int i = 2; i*i <= num; i++)
                 if (num % i == 0)
                     return false;
             return true;
